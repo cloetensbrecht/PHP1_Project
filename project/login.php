094a365b044380b3ad5757990b49184f75eb742d
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-04-08 19:41:43 +0200

    create login form + error message

--- project/login.php
+++ project/login.php
@@ -7,6 +7,35 @@
     <title>Document</title>
 </head>
 <body>
-    
+<div class="login">
+		<div class="form form--login">
+			<form action="" method="post">
+				<img id="logo" src="#" alt="">
+				<h2 form__title>Login</h2>
+                
+				<?php if (isset($error)): ?>
+					<div class="form__error">
+						<p>Sorry, we can't log you in with that email address and password. Can you try again?</p>
+					</div>
+				<?php endif; ?>
+
+				<div class="form__field">
+					<label for="email">Email</label>
+					<input type="text" name="email" placeholder="email">
+				</div>
+				<div class="form__field">
+					<label for="password">Password</label>
+					<input type="password" name="password" placeholder="password">
+				</div>
+
+				<div class="form__field">
+					<input type="submit" value="Log in!" class="btn btn--primary">
+				</div>
+				<div>
+					<p>No account yet? <a href="register.php"> Sign up first</a></p>
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
