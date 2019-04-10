0486446f63f284e4eb256eb458851095b45025a8
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-04-10 14:32:38 +0200

    missing . & start session

--- project/bootstrap.php
+++ project/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
+    // start session
+    session_start();
 
     // load of classes
     spl_autoload_register(function($class){
-    require_once(__DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . $class . "class.php");
+    require_once(__DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . $class . ".class.php");
     });
