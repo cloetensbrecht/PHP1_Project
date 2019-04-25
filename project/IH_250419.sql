5c4cedf97f23f23acd557b82a08f42f00e712867
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-04-25 11:15:50 +0200

    Update IH_250419.sql

--- project/IH_250419.sql
+++ project/IH_250419.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 25, 2019 at 09:12 AM
+-- Generation Time: Apr 25, 2019 at 09:15 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -13,6 +13,104 @@ SET time_zone = "+00:00";
 --
 -- Database: `InspirationHunter`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `tags` varchar(255) NOT NULL,
+  `time` datetime NOT NULL,
+  `likes_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `user_id`, `foto`, `tags`, `time`, `likes_id`) VALUES
+(1, 234466, 'https://www.db-m.nl/wp-content/uploads/2014/10/geenfoto-1024x683.jpg', '#image #profiel #me', '2019-04-24 00:00:00', 3),
+(2, 234466, 'https://www.db-m.nl/wp-content/uploads/2014/10/geenfoto-1024x683.jpg', '#image #profiel #me', '2019-04-24 00:00:00', 3),
+(3, 324, 'https://dsocdn.akamaized.net/Assets/Images_Upload/2017/07/13/c5d28dac-67b9-11e7-8c3c-11de46a62a49_web_translate_-1.1e-5_-4e-6__scale_0.4669987_0.4669987__.jpg?maxheight=416&maxwidth=568', 'aap profiel #myfaceispretty', '2019-04-17 00:00:00', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `POSTID` int(3) NOT NULL,
+  `POSTTITLE` varchar(100) NOT NULL,
+  `POSTDETAILS` varchar(10000) NOT NULL,
+  `POSTLINK` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `postText` varchar(255) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `postText`, `time`) VALUES
+(1, 'dit is de titel', 'bla bla bla', '2019-04-24 17:39:32'),
+(2, 'PHP Lover', 'i love php ', '2019-04-24 17:39:57'),
+(3, 'PHP lover', 'We love php', '2019-04-24 17:53:24'),
+(4, 'Ajax', 'we love ajax', '2019-04-24 17:53:24'),
+(5, 'PHP lover', 'We love php', '2019-04-24 17:53:30'),
+(6, 'Ajax', 'we love ajax', '2019-04-24 17:53:30'),
+(7, 'PHP lover', 'We love php', '2019-04-24 17:53:43'),
+(8, 'Ajax', 'we love ajax', '2019-04-24 17:53:43'),
+(9, 'PHP lover', 'We love php', '2019-04-24 17:53:43'),
+(10, 'Ajax', 'we love ajax', '2019-04-24 17:53:43'),
+(11, 'PHP lover', 'We love php', '2019-04-24 17:53:43'),
+(12, 'Ajax', 'we love ajax', '2019-04-24 17:53:43'),
+(13, 'PHP lover', 'We love php', '2019-04-24 17:53:43'),
+(14, 'Ajax', 'we love ajax', '2019-04-24 17:53:43'),
+(15, 'PHP lover', 'We love php', '2019-04-24 17:53:53'),
+(16, 'Ajax', 'we love ajax', '2019-04-24 17:53:53'),
+(17, 'PHP lover', 'We love php', '2019-04-24 17:53:53'),
+(18, 'Ajax', 'we love ajax', '2019-04-24 17:53:53'),
+(19, 'PHP lover', 'We love php', '2019-04-24 17:53:53'),
+(20, 'Ajax', 'we love ajax', '2019-04-24 17:53:53'),
+(21, 'PHP lover', 'We love php', '2019-04-24 17:53:53'),
+(22, 'Ajax', 'we love ajax', '2019-04-24 17:53:53'),
+(23, 'PHP lover', 'We love php', '2019-04-24 17:54:03'),
+(24, 'Ajax', 'we love ajax', '2019-04-24 17:54:03'),
+(25, 'PHP lover', 'We love php', '2019-04-24 17:54:03'),
+(26, 'Ajax', 'we love ajax', '2019-04-24 17:54:03'),
+(27, 'PHP lover', 'We love php', '2019-04-24 17:54:03'),
+(28, 'Ajax', 'we love ajax', '2019-04-24 17:54:03'),
+(29, 'PHP lover', 'We love php', '2019-04-24 17:54:03'),
+(30, 'Ajax', 'we love ajax', '2019-04-24 17:54:03');
 
 -- --------------------------------------------------------
 
@@ -52,6 +150,24 @@ INSERT INTO `users` (`id`, `email`, `password`) VALUES
 --
 
 --
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -60,6 +176,24 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `users`
