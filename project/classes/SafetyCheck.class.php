cb58025bb29c86900b0b65187352668c7fc31e6a
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-05-05 14:51:08 +0200

    Update SafetyCheck.class.php

--- project/classes/SafetyCheck.class.php
+++ project/classes/SafetyCheck.class.php
@@ -1,11 +1,14 @@
-<?php 
-    class SafetyCheck{
+<?php
+
+    class SafetyCheck
+    {
         // check email - isValidEmail
-        public static function isValidEmail($email){
-            if(filter_var($email, FILTER_VALIDATE_EMAIL)){ 
+        public static function isValidEmail($email)
+        {
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 return true;
             }
+
             return false;
         }
-
-    }   
+    }
