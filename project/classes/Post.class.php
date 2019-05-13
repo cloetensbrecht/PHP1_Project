dfc5cf88a653840d209b16dfaadae10cb584a07b
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-05-13 20:57:38 +0200

    Update Post.class.php

--- project/classes/Post.class.php
+++ project/classes/Post.class.php
@@ -1,8 +1,37 @@
 <?php
     require_once 'bootstrap.php';
 
-    class Post extends User
+    class Post
     {
+        public static function getAll()
+        {
+            $emailCheck = $_SESSION['id'];
+
+            $conn = Db::getInstance(); // db connection
+            $result = $conn->prepare('SELECT id FROM users WHERE email= :email;');
+            $result->bindParam(':email', $emailCheck);
+            $result->execute();
+            $id = $result->fetch(PDO::FETCH_COLUMN);
+
+            //echo 'dit is id'.$id;
+
+            $result = $conn->prepare('SELECT * FROM posts, users, friends
+            WHERE users.id = posts.user_id
+            AND users.id = friends.user_id_friend
+            AND friends.user_id = :id
+            ORDER BY posts.time DESC'); //LIMIT $row, $rowperpage'
+            $result->bindParam(':id', $id);
+            $result->execute();
+
+            return $resultOfPosts = $result->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        /*
+    user_id		    = mijn id           // LINK users.id = friends.user_id
+    user_id_friend	= user id die ik volg = vriend // LINK users.id = friends.user_id_friend
+    status			= 1 = vriend / 0 = “ont-vriend”
+    */
+
         public static function loadMore($limit)
         {
             $conn = Db::getInstance(); // db connection
