4b517713f855ae635245183011a49c743f24beec
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-05-13 20:58:59 +0200

    FEATURE 7 - loadMore
    doesnt work yet

--- project/loadmorePosts.php
+++ project/loadmorePosts.php
@@ -3,30 +3,47 @@
 require_once 'bootstrap.php';
 $conn = Db::getInstance(); // db connection
 
-//sanitize post value
-//$pageNumber = filter_var($_POST['page'], FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_STRIP_HIGH);
-$pageNumber = 1;
+$row = $_POST['row'];
+$rowperpage = 3;
 
-$PostsPerPage = 3;
-//get current starting point of records
-$position = (($pageNumber - 1) * $PostsPerPage);
-//fetch records using page position and item per page.
+echo 'row'.$row;
 
-$result = $conn->prepare('SELECT * FROM posts, users, friends
-WHERE users.id = posts.user_id
-AND users.id = friends.user_id_friend
-AND friends.user_id = :id
-ORDER BY posts.time DESC LIMIT :p, :ppp');
-$result->bindParam(':p', $position);
-$result->bindParam(':ppp', $PostsPerPage);
+// id ophalen uit db
+$emailCheck = $_SESSION['id'];
+
+$conn = Db::getInstance(); // db connection
+$result = $conn->prepare('SELECT id FROM users WHERE email= :email');
+$result->bindParam(':email', $emailCheck);
+$result->execute();
+$id = $result->fetch(PDO::FETCH_COLUMN);
+
+/* FEATURE 5 - laatste 20 posts  */ // overzicht geuploade img
+$result = $conn->prepare("SELECT * FROM posts, users, friends
+WHERE users.id=posts.user_id 
+AND users.id=friends.user_id_friend 
+AND friends.user_id= :id ORDER BY posts.time DESC LIMIT $row, $rowperpage");
 $result->bindParam(':id', $id);
 $result->execute();
-$resultOfPosts = $result->fetchAll();
+$resultOfPosts = $result->fetchAll(PDO::FETCH_ASSOC);
 
-//var_dump($resultOfPosts);
+$html = '';
 
-foreach ($resultOfPosts as $r => $row) {
-    echo '<p><strong>'.$row['username'].'</strong></p>';
-    echo '<p>'.$row['description'].'</p>';
-    echo '<p>'.$row['filter'].'</p>'; // FEATURE 16
+foreach ($resultOfPosts as $rop => $r) {
+    $id = $r['id'];
+    $images = $r['image'];
+    $description = $r['description'];
+    $username = $r['username'];
+    $filter = $r['filter'];
+
+    // create HTML
+
+    $html .= "<div id='img_div'> ";
+    $html .= "<div class='".$filter."'><img src='postImages/".$images."'> </div>";
+    $html .= '<p><strong>'.$username.'</strong></p>';
+    $html .= '<p>'.$description.'</p>';
+    //$html .= '<p>'.$timeStatus.'</p>'; // FEATURE 13
+    $html .= '<p>'.$filter.'</p>'; // FEATURE 16
+    $html .= '</div>';
 }
+
+echo $html;
