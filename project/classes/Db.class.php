7873357f68d9fa0fdac5bf019b224c0c976dfd30
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-05-15 18:59:35 +0200

    DB online - for online hosting

--- project/classes/Db.class.php
+++ project/classes/Db.class.php
@@ -1,19 +1,23 @@
-<?php 
-    // om connectie te maken > $conn = Db::getInstance(); // DRY 
-    abstract class Db {
+<?php
+
+    // om connectie te maken > $conn = Db::getInstance(); // DRY
+    abstract class Db
+    {
         private static $conn;
-        public static function getInstance(){
-            if(self::$conn != null){
-                echo "🚫";
+
+        public static function getInstance()
+        {
+            if (self::$conn != null) {
+                echo '🚫';
                 // connection found, return connection
                 return self::$conn;
-            } else{
+            } else {
                 //$config = parse_ini_file('config/config.ini');
                 //self::$conn = new PDO('mysql:host=localhost;dbname=' . $config['db_name'], $config['db_user'], $config['db_password'] );
-                self::$conn = new PDO("mysql:host=localhost;dbname=InspirationHunter", "root", "root");
+                self::$conn = new PDO('mysql:host=localhost;dbname=InspirationHunter', 'root', 'root');
+                //self::$conn = new PDO('mysql:host=localhost;dbname=epiz_23910941_travelinspiration', 'epiz_23910941', 'dusjac-3duzzY-rebkef');
+
                 return self::$conn;
             }
         }
     }
-
-    
