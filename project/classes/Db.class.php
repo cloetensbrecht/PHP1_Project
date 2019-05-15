c00a1e64ef7572a0131125df1f0c57bcbdd3a4e8
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-05-15 22:48:50 +0200

    db  / host=localhost

--- project/classes/Db.class.php
+++ project/classes/Db.class.php
@@ -16,7 +16,8 @@
                 //$config = parse_ini_file('config/config.ini');
                 //self::$conn = new PDO('mysql:host=localhost;dbname=' . $config['db_name'], $config['db_user'], $config['db_password'] );
                 self::$conn = new PDO('mysql:host=localhost;dbname=travelinspiration', 'root', 'root');
-                //self::$conn = new PDO('mysql:host=sql307.epizy.com;dbname=epiz_23910941_travelinspiration', 'epiz_23910941', 'dusjac-3duzzY-rebkef');
+                //self::$conn = new PDO('mysql:host=localhost;dbname=epiz_23910941_travelinspiration', 'epiz_23910941', 'dusjac-3duzzY-rebkef');
+                //self::$conn = new PDO('mysql:host=localhost;dbname=epiz_23910941_travelinspiration', 'epiz_23910941', 'EGmZn9IOQ6lBYa1');
 
                 return self::$conn;
             }
