7488ef4bea6fdac4bac906d3231ea48fa3bb1285
cloetensbrecht <info@cloetensbrecht.be>
2019-05-20 23:26:21 +0200

    Create unfollow.php

---
+++ project/ajax/unfollow.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__.'/../bootstrap.php';
+
+function unFollow() {
+  if (empty($_POST['id']))
+    return [
+     'status' => 'error',
+     'message' => 'The friend id is missing',
+    ];
+
+  $friendId = $_POST['id'];
+  if (!User:isFollowing($friendId))
+    return [
+      'status' => 'error',
+      'message' => 'You are not following this friend'
+    ];
+
+  if (!User::unFollow($friendId))
+    return [
+      'status' => 'error',
+      'message' => 'You are still following this friend'
+    ];
+  
+  return [
+    'status' => 'success',
+    'message' => 'You unfollowed this friend'
+  ];
+}
+
+exit(json_encode(unfollow()));
